b88d5b8ebe0746fcf9e7c0df5863fc2ee0c92855
lucreziascaffidi <lucrezia.scaffidi@gmail.com>
2024-10-08 18:28:44 +0200

    feat: added parse

--- js/master.js
+++ js/master.js
@@ -2,23 +2,23 @@ console.log('Hello world!')
 
 // Inizializzo la variabile relativa ai KM che l'utente deve percorrere per il viaggio
 // le assegno il numero che l'utente inserisce nel prompt in cui appare la domanda "Quanti km devi percorrere?"
+const userPromptKM = prompt('Quanti km devi percorrere?');
 
-const toTravelKM = prompt('Quanti km devi percorrere?');
-console.log(
-    `
-    L'utente deve percorrere ${toTravelKM} KM.
-    `
-);
+// trasformo il risultato stringa in numero
+const toTravelKM = parseFloat(userKM);
+
+// stampo il numero di KM che deve percorrere l'utente
+console.log(`L'utente deve percorrere ${toTravelKM} KM.`);
 
 // Inizializzo la variabile relativa all'età dell'utente che deve fare il viaggio
 // le assegno il numero che l'utente inserisce nel prompt in cui appare la domanda "Quanti anni hai?"
+const userPromptAge = prompt('Quanti anni hai?');
 
-const userAge = prompt('Quanti anni hai?');
-console.log(
-    `
-    L'utente ha ${userAge} anni.
-    `
-);
+// trasformo il risultato stringa in numero
+const userAge = parseInt(userPromptAge)
+
+// stampo il numero età che inserisce l'utente
+console.log(`L'utente ha ${userAge} anni.`);
 
 // Inizializzo la variabile relativa al prezzo del biglietto per 1 Km
 // e le assegno il numero 0.21 considerando che il costo è di 0.21€ per 1 km. 
@@ -52,31 +52,19 @@ const ticketPriceOver65 = (ticketPrice * 40) / 100;
 if (under18) {
 
     // allora il costo del biglietto è scontato del 20% quindi faccio apparire questa frase
-    console.log(
-        `
-            Il costo totale del biglietto è di ${ticketPriceUnder18.toFixed(2)}€. Scontato del 20% per i minorenni.
-        `
-    )
+    console.log(`Il costo totale del biglietto è di ${ticketPriceUnder18.toFixed(2)}€. Scontato del 20% per i minorenni.`)
 
     //altrimenti se l'età è maggiore di 65 anni  
 } else if (over65) {
 
     // allora il costo del biglietto è scontato del 40% e faccio apparire questa frase
-    console.log(
-        `
-            Il costo totale del biglietto è di ${ticketPriceOver65.toFixed(2)}€. Scontato del 40% per gli over 65.
-        `
-    )
+    console.log(`Il costo totale del biglietto è di ${ticketPriceOver65.toFixed(2)}€. Scontato del 40% per gli over 65.`)
 
     // altrimenti l'età è sicuramente compresa tra i 18 e i 65 anni
 } else {
 
     // allora l'utente non ha diritto a nessuno sconto e faccio apparire questa frase
-    console.log(
-        `
-            Il costo totale del biglietto è di ${ticketPrice.toFixed(2)}€. L'utente non ha diritto a nessuno sconto.
-        `
-    )
+    console.log(`Il costo totale del biglietto è di ${ticketPrice.toFixed(2)}€. L'utente non ha diritto a nessuno sconto.`)
 
 }
 
